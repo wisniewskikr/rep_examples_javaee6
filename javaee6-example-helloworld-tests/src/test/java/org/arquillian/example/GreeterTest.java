86aa35155638a3f5b056754de1dcc6a5ba956e26
Krzysztof Wisniewski <wisniewskikr@gmail.com>
2014-02-26 16:31:40 +0100

    Add frame of simple test in project javaee6-example-helloworld-tests

---
+++ javaee6-example-helloworld-tests/src/test/java/org/arquillian/example/GreeterTest.java
@@ -0,0 +1,48 @@
+package org.arquillian.example;
+
+import java.io.File;
+
+import javax.inject.Inject;
+ 
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import pl.kwi.services.NameService;
+ 
+@RunWith(Arquillian.class)
+public class GreeterTest {
+     
+    @Inject
+    NameService greeter;
+     
+    @Deployment
+    public static WebArchive createDeployment() {
+        File[] lib = Maven.resolver()
+                .resolve("org.jboss.weld.servlet:weld-servlet:2.1.0.Final")
+                .withTransitivity().as(File.class);
+         
+        WebArchive jar =  ShrinkWrap.create(WebArchive.class)
+            .addClass(NameService.class)
+            .addAsManifestResource("arquillian.xml")
+            .addAsLibraries(lib)
+            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+            .setWebXML("web.xml")
+            ;
+         
+        System.out.println(jar.toString(true));
+         
+        return jar;
+    }
+     
+    @Test
+    public void should_create_greeting() {
+        
+    }
+}
