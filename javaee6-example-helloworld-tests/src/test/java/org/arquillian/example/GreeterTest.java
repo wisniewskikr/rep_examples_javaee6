ed415ff7f650f0fb27814cfb41c1cb7b777c1752
Krzysztof Wisniewski <wisniewskikr@gmail.com>
2014-02-26 17:00:44 +0100

    Improving intg tests in project javaee6-example-helloworld-tests

--- javaee6-example-helloworld-tests/src/test/java/org/arquillian/example/GreeterTest.java
+++ javaee6-example-helloworld-tests/src/test/java/org/arquillian/example/GreeterTest.java
@@ -6,19 +6,27 @@ import javax.inject.Inject;
  
 
 
+
+
+
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Filters;
+import org.jboss.shrinkwrap.api.GenericArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.ScopeType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import pl.kwi.tmp.Tmp;
- 
 @RunWith(Arquillian.class)
 public class GreeterTest {
+	
+	private static final String WEBAPP_SRC = "src/main/webapp";  
      
     @Inject
 //    Tmp greeter;
@@ -26,20 +34,24 @@ public class GreeterTest {
     @Deployment
     public static WebArchive createDeployment() {
         File[] lib = Maven.resolver()
-                .resolve("org.jboss.weld.servlet:weld-servlet:2.1.0.Final")
+        		.loadPomFromFile("pom.xml")
+        		.importDependencies(ScopeType.COMPILE)
+                .resolve()
                 .withTransitivity().as(File.class);
          
-        WebArchive jar =  ShrinkWrap.create(WebArchive.class)
-            .addClass(Tmp.class)
+        WebArchive war =  ShrinkWrap.create(WebArchive.class)
+        	.addPackages(true, "pl.kwi")
             .addAsManifestResource("arquillian.xml")
             .addAsLibraries(lib)
-            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-            .setWebXML("web.xml")
-            ;
+            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+        
+        war.merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class)  
+        	    .importDirectory(WEBAPP_SRC).as(GenericArchive.class),  
+        	    "/", Filters.includeAll());  
          
-        System.out.println(jar.toString(true));
+        System.out.println(war.toString(true));
          
-        return jar;
+        return war;
     }
      
     @Test
